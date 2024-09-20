7946d1bd4d12f4b02228f47f8c4a40a095bf46b5
Ktang920914 <jay.ang920914@gmail.com>
2024-09-21 00:09:33 +0800

    create profile UI

--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {useSelector} from 'react-redux'
+import {TextInput, Button} from 'flowbite-react'
 
 const DashProfile = () => {
+
+  const {currentUser} = useSelector(state => state.user)
+
   return (
-    <div>DashProfile</div>
+    <div className='max-w-lg mx-auto p-3 w-full'>
+      <h1 className='text-center my-7 text-3xl font-semibold'>Profile</h1>
+
+      <form className='flex flex-col gap-4'>
+        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+        <img src={currentUser.profilePicture} alt='' className='rounded-full w-full h-full border-8 border-[lightgray] object-cover'/>
+        </div>
+
+        <TextInput type='text' id='username' placeholder='Username' defaultValue={currentUser.username}/>
+        <TextInput type='email' id='email' placeholder='Email' defaultValue={currentUser.email}/>
+        <TextInput type='password' id='password' placeholder='Password' defaultValue={currentUser.password}/>
+
+        <Button type='submit' gradientDuoTone='purpleToPink' outline>Update</Button>
+      </form>
+
+      <div className='text-red-500 flex justify-between mt-5'>
+        <span className='cursor-pointer'>Delete Account</span>
+        <span className='cursor-pointer'>Sign out</span>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/ThemeProvider.jsx
+++ frontend/src/components/ThemeProvider.jsx
@@ -6,7 +6,7 @@ const ThemeProvider = ({children}) => {
     const {theme} = useSelector(state => state.theme)
   return (
     <div className={theme}>
-        <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-black min-h-screen'>
+        <div className='bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-800 min-h-screen'>
             {children}
         </div>
     </div>
