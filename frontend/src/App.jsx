1498268b90b30bccdfe635e889ab7b6b7a3a83df
Ktang920914 <jay.ang920914@gmail.com>
2024-09-20 21:48:54 +0800

    create private route for dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header'
 import FooterComp from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 const App = () => {
   return (
@@ -16,7 +17,11 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/about' element={<About />}/>
+
+        <Route element={<PrivateRoute />}>
         <Route path='/dashboard' element={<Dashboard />}/>
+        </Route>
+        
         <Route path='/projects' element={<Projects />}/>
         <Route path='/sign-in' element={<SignIn />}/>
         <Route path='/sign-up' element={<SignUp />}/>
