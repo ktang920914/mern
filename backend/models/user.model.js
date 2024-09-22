16ede7822461a84510baa1b53e23872206863262
Ktang920914 <jay.ang920914@gmail.com>
2024-09-22 15:56:21 +0800

    add isadmin functionality to user

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema(
     profilePicture:{
       type: String,
       default: 'https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg',
+    },
+    isAdmin:{
+      type: Boolean,
+      default: false
     }
 },{ timestamps: true }
 )
