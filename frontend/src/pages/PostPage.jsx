8669c7b907d8e97430f0a2b8b71287656e134a20
Ktang920914 <jay.ang920914@gmail.com>
2024-09-24 13:09:43 +0800

    add recent articles to post page ui

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import CallToAction from '../components/CallToAction'
 import CommentSection from '../components/CommentSection'
+import PostCard from '../components/PostCard'
 
 const PostPage = () => {
 
@@ -10,6 +11,7 @@ const PostPage = () => {
     const [loading,setLoading] = useState(true)
     const [error,setError] = useState(false)
     const [post,setPost] = useState(null)
+    const [recentPosts,setRecentPosts] = useState(null)
 
 useEffect(() => {
     const fetchPost = async () => {
@@ -35,6 +37,23 @@ useEffect(() => {
     fetchPost()
 },[postSlug])
 
+useEffect(() => {
+    try {
+        const fetchRecentPosts = async () => {
+            const res = await fetch('/api/post/getposts?limit=4')
+            const data = await res.json()
+            if(data.success !== false){
+                setRecentPosts(data.posts)
+            }
+        }
+        fetchRecentPosts()
+    } catch (error) {
+        console.log(error.message)
+    }
+
+
+})
+
 if (loading) return <div className='flex justify-center items-center min-h-screen'> 
 <Spinner size='xl'/> </div>
 
@@ -50,7 +69,7 @@ if (loading) return <div className='flex justify-center items-center min-h-scree
         </Link>
 
         <img src={post && post.image} alt={post && post.title}
-        className='mt-10 p-3 max-h-[600px] w-full object-cover'/>
+        className='mt-10 p-3 max-h-[600px] w-full object-cover rounded-3xl'/>
 
         <div className='flex justify-between p-3 border-b border-slate-300 w-full mx-auto max-w-2xl text-xs'>
             <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
@@ -65,6 +84,21 @@ if (loading) return <div className='flex justify-center items-center min-h-scree
             <CallToAction />
         </div>
         <CommentSection postId={post._id}/>
+
+        <div className='flex flex-col justify-center items-center mb-5'>
+            <h1 className='text-xl mt-5'>
+                Recent articles
+            </h1>
+
+            <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+                {
+                    recentPosts && recentPosts.map((post) => (
+                        <PostCard key={post._id} post={post}/>
+                    ))
+
+                }
+            </div>
+        </div>
     </main>
   )
 }
