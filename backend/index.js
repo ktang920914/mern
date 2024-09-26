5a36e7c23a365db1b979c2f38cc75047609d1fd3
Ktang920914 <jay.ang920914@gmail.com>
2024-09-26 20:19:52 +0800

    add pre installation

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ mongoose.connect(process.env.MONGO)
 .catch((err) => console.log(err))
 
 const __dirname = path.resolve()
-app.use(express.static(path.join(__dirname, '/client/dist')))
+app.use(express.static(path.join(__dirname, '/frontend/dist')))
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname,'frontend' ,'dist', 'index.html'))
 })
